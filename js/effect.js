5e3c79743d3331d6294e6b24bc7dd9ab5873a718
Tim Yarmatov <yarmatoff@yandex.ru>
2021-03-17 22:57:45 +0300

    Галлерея фильтров

---
+++ js/effect.js
@@ -0,0 +1,33 @@
+
+/*
+1. Создадим массив с названиями классов (СSS) назовем его effectType;
+2. Получим доступ к нуныжным  элементам;
+3. Cоздадим функцию
+*/
+// Создадим массив с названиями классов (СSS) эффектов
+const effects = [
+  'effects__preview--none',
+  'effects__preview--chrome',
+  'effects__preview--sepia',
+  'effects__preview--marvin',
+  'effects__preview--phobos',
+  'effects__preview--heat',
+];
+
+// Доступ к элементам
+const uploadForm = document.querySelector('.img-upload__form');
+const effectsRadio = uploadForm.querySelectorAll('.effects__radio');
+const picturePreview = uploadForm.querySelector('.img-upload__preview');
+//const effectLevel = uploadForm.querySelector('.effect-level');
+
+//Создаем функцию
+
+const addEffectsClickHandler = (effectsRadio, effects) => {
+  effectsRadio.addEventListener('click', () => {
+    picturePreview.classList.add(effects);
+  });
+}
+
+for (let i = 0; i < effectsRadio.length; i++) {
+  addEffectsClickHandler(effectsRadio[i], effects[i]);
+}
--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import './render.js';
 import './uploading.js';
 import './resize.js';
+import './effect.js';
 
 
